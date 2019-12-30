72de9798f63643d6b49daac61dffdb8a400886de
octopusion <30819807+octopusion@users.noreply.github.com>
2019-12-30 11:50:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
  <?php
 require_once 'vendor/autoload.php';
+require_once "./random_string.php";
 
 use MicrosoftAzure\Storage\Blob\BlobRestProxy;
 use MicrosoftAzure\Storage\Common\Exceptions\ServiceException;
